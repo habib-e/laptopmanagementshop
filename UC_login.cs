a778283c69a260a08cba7ccf378999b89a1113cb
habib <basharmd978@gmail.com>
2021-03-13 11:28:31 +0600

    Add project files.

---
+++ UC_login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laptopmanagementshop
+{
+    public partial class UC_login : UserControl
+    {
+        public UC_login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void UC_login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToShowLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
